771680e38f90dc9093b055fa8585147fad611d26
Roland Tritsch <rtritsch@gilt.com>
2013-08-14 19:52:34 +0100

    210.immutable_collections: Using the App trait now

--- 210.immutable_collections/src/main/scala/scalax/MyList.scala
+++ 210.immutable_collections/src/main/scala/scalax/MyList.scala
@@ -1,13 +1,11 @@
 package scalax
 
-object MyList {
-  def main(args: Array[String]): Unit = {
-    assert(args.size >= 1, "Usage: MyList 1 2 3 ...")
+object MyList extends App {
+  assert(args.size >= 1, "Usage: MyList 1 2 3 ...")
 
-    val digits = args.map(_.toInt).toList
-    val reversedDigits = reverse(digits)
-    println(reversedDigits.mkString(" "))
-  }
+  val digits = args.map(_.toInt).toList
+  val reversedDigits = reverse(digits)
+  println(reversedDigits.mkString(" "))
 
   private def reverse(in: List[Int]): List[Int] = {
     List()
--- 210.immutable_collections/src/main/scala/solution1/MyList.scala
+++ 210.immutable_collections/src/main/scala/solution1/MyList.scala
@@ -2,22 +2,20 @@ package solution1
 
 import scala.collection.mutable.MutableList
 
-object MyList {
-  def main(args: Array[String]): Unit = {
-    assert(args.size >= 1, "Usage: MyList 1 2 3 ...")
+object MyList extends App {
+  assert(args.size >= 1, "Usage: MyList 1 2 3 ...")
 
-    val digits = new MutableList[Int]()
-    for(i <- 0 until args.size) {
-      digits += args(i).toInt
-    }
-
-    val reversedDigits = MutableList[Int]()
-    reverse(digits, reversedDigits)
-    for(i <- 0 until reversedDigits.size) {
-      print(reversedDigits(i) + " ")
-    }
-    println()
+  val digits = new MutableList[Int]()
+  for(i <- 0 until args.size) {
+    digits += args(i).toInt
   }
+
+  val reversedDigits = MutableList[Int]()
+  reverse(digits, reversedDigits)
+  for(i <- 0 until reversedDigits.size) {
+    print(reversedDigits(i) + " ")
+  }
+  println()
 
   private def reverse(in: MutableList[Int], out: MutableList[Int]): Unit = {
     for(i <- 0 until in.size) {
