c0f6bb1a33c93ac531522168513db711a22318ff
Roland Tritsch <rtritsch@gilt.com>
2013-08-16 16:49:08 +0100

    210: Using mutable.ListBuffer

--- 210.immutable_collections/src/main/scala/solution1/MyList.scala
+++ 210.immutable_collections/src/main/scala/solution1/MyList.scala
@@ -1,23 +1,23 @@
 package solution1
 
-import scala.collection.mutable.MutableList
+import scala.collection.mutable
 
 object MyList extends App {
   assert(args.size >= 1, "Usage: MyList 1 2 3 ...")
 
-  val digits = new MutableList[Int]()
+  val digits = new mutable.ListBuffer[Int]()
   for(i <- 0 until args.size) {
     digits += args(i).toInt
   }
 
-  val reversedDigits = MutableList[Int]()
+  val reversedDigits = mutable.ListBuffer[Int]()
   reverse(digits, reversedDigits)
   for(i <- 0 until reversedDigits.size) {
     print(reversedDigits(i) + " ")
   }
   println()
 
-  private def reverse(in: MutableList[Int], out: MutableList[Int]): Unit = {
+  private def reverse(in: mutable.ListBuffer[Int], out: mutable.ListBuffer[Int]): Unit = {
     for(i <- 0 until in.size) {
       in(i) +=: out
     }
