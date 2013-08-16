28a3184f03b41fe3abd2fecda00eeef8381a2545
Roland Tritsch <rtritsch@gilt.com>
2013-08-16 16:21:20 +0100

    Simplified solution

--- 135.classes_traits_objects/src/main/scala/solution/ClassesTraitsObjects.scala
+++ 135.classes_traits_objects/src/main/scala/solution/ClassesTraitsObjects.scala
@@ -1,45 +1,41 @@
-package solution;
+package solution
 
-import collection.immutable._
 import scala.collection.mutable.ListBuffer
 
 object Garage {
   val vehicles = new ListBuffer[Vehicle]()
 
-  def addVehicle(vehicle: Vehicle) =  { vehicles += vehicle; this }
-  def removeVehicle(vehicle: Vehicle) = { vehicles -= vehicle; this }
-  def getVehicles() : List[Vehicle] = vehicles.toList
+  def addVehicle(vehicle: Vehicle): Unit = vehicles += vehicle
+  def removeVehicle(vehicle: Vehicle): Unit = vehicles -= vehicle
+  def getVehicles: List[Vehicle] = vehicles.toList
 }
 
 case class Vehicle(make: String, wheelCount : Int)
-class Car(make: String) extends Vehicle(make, 4) 
-class Truck(make: String) extends Vehicle(make, 18) 
+class Car(make: String) extends Vehicle(make, 4)
+class Truck(make: String) extends Vehicle(make, 18)
 
 trait Convertible {
-  def openTop() { println("Top is open") }
-  def closeTop() { println("Top is closed") }
+  def openTop: Unit = println("Top is open")
+  def closeTop: Unit = println("Top is closed")
 }
 
 class SportsCar(make: String) extends Vehicle(make, 4) with Convertible
 
-object ClassesTraitsObjects {
-  def main(args: Array[String]): Unit = {
-    val garage = Garage
-    garage.addVehicle(new Car("BMW"))
-    garage.addVehicle(new SportsCar("Maserati"))
-    garage.addVehicle(new Truck("Scania"))
+object ClassesTraitsObjects extends App {
+  val garage = Garage
+  garage.addVehicle(new Car("BMW"))
+  garage.addVehicle(new SportsCar("Maserati"))
+  garage.addVehicle(new Truck("Scania"))
 
-    garage.getVehicles foreach {
-      v => println("This vehicle is a " + v.make + " and has " + v.wheelCount + " wheels");
-
-      v match {
-        case c:Convertible => {
-                  println("Oooh, this vehicle is a convertible. Let's play with the top!")
-                  c.openTop()
-                  c.closeTop()
-        }
-        case _ =>
+  garage.getVehicles.foreach(v => {
+    println("This vehicle is a " + v.make + " and has " + v.wheelCount + " wheels")
+    v match {
+      case c: Convertible => {
+        println("Oooh, this vehicle is a convertible. Let's play with the top!")
+        c.openTop
+        c.closeTop
       }
-    }
-  }
+      case _ => println("Not a convertible")
+    }}
+  )
 }
