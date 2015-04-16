b7c90b2a6cefdecf5208485d55dbf09ac1705ce9
Maciek Sufa <maciej.sufa@gmail.com>
2015-04-16 15:12:02 +0100

    ex 230 solution

--- 230.pattern_matching/src/main/scala/com/gilt/pattern/matching/scala/Example.scala
+++ 230.pattern_matching/src/main/scala/com/gilt/pattern/matching/scala/Example.scala
@@ -13,7 +13,13 @@ case class Food(price: Int) extends Item
 case class ShoppingCart(items: Set[Item]) {
   lazy val computeTax = {
     // books are 5%, clothes are 8%, food is 9% (see TaxVisitor.java)
-    ???
+    var total: Double = 0
+    for (item <- items) item match {
+      case book: Book => total += book.price * 0.05
+      case clothes: Clothes => total += clothes.price * 0.08
+      case food: Food => total += food.price * 0.09
+    }
+    total
   }
 }
 
