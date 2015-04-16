012e7fc4eca2319905428f76172e09d76b81baa2
Maciek Sufa <maciej.sufa@gmail.com>
2015-04-16 16:56:53 +0100

    ex 410 solution

--- 410.monads/src/main/scala/monads/scalax/Words.scala
+++ 410.monads/src/main/scala/monads/scalax/Words.scala
@@ -5,11 +5,15 @@ import scala.io.Source
 object Words extends App {
   val keywords = Source.fromURL(getClass.getResource("/keywords.txt")).getLines.toSet
 
-  val words: List[String] = ???
+  val words: List[String] = args.toList
 
-  val foundKeywords: List[String] = ???
+  val foundKeywords: List[String] = for {
+    word <- words if keywords.contains(word.toLowerCase)
+  } yield word
   println("All keywords: " + foundKeywords.mkString("[",",","]"))
 
-  val upperCaseWords: List[String] = ???
+  val upperCaseWords: List[String] = for {
+    word <- words
+  } yield word.toUpperCase
   println("All words in uppercase: " + upperCaseWords.mkString("[",",","]"))
 }
