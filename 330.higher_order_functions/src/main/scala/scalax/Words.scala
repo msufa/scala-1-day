bc450069e971212157e2293f327103647c3279a0
Maciek Sufa <maciej.sufa@gmail.com>
2015-04-17 00:24:11 +0100

    ex 330 solution

--- 330.higher_order_functions/src/main/scala/scalax/Words.scala
+++ 330.higher_order_functions/src/main/scala/scalax/Words.scala
@@ -5,15 +5,15 @@ object Words extends App {
     arg.split(" ").filter(word => word.length > 0)
   }
 
-  val wordsByFirstLetter: Map[Char, List[String]] = ???
+  val wordsByFirstLetter: Map[Char, List[String]] = words.groupBy(word => word.head.toLower)
   println("Words by first letter: " + wordsByFirstLetter)
 
-  val even, odd: (List[String], List[String]) = ???
+  val (even, odd): (List[String], List[String]) = words.partition(word => word.size % 2 == 0)
   println("Words with even length: " + even)
   println("Words with odd length: " + odd)
 
-  val wordHistogram: Map[String, Int] = words.foldLeft(???) {
-    ???
-  }
+  val wordHistogram: Map[String, Int] = words.foldLeft(
+    Map[String, Int]())((hist, word) => hist + (word.toLowerCase -> (hist.getOrElse(word.toLowerCase, 0) + 1)))
+
   println("Word frequencies: " + wordHistogram)
 }
