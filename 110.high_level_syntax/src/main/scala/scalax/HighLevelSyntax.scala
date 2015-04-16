bf877bda83c7ecbc7a85c5e82dde4df675b81190
Maciek Sufa <maciej.sufa@gmail.com>
2015-04-16 11:06:40 +0100

    ex 110 solution

--- 110.high_level_syntax/src/main/scala/scalax/HighLevelSyntax.scala
+++ 110.high_level_syntax/src/main/scala/scalax/HighLevelSyntax.scala
@@ -1,6 +1,15 @@
 package scalax
 
 object HighLevelSyntax extends App {
-  println("Hello World!")
-  // println(buildString(hello, world))
+  def getParameters(args: Array[String]): Option[String] = {
+    if (args.length == 1) Some(args(0))
+    else None
+  }
+
+  def buildString(one: String, two: String): String = s"$one $two"
+
+  val hello = "Hello"
+  val world = getParameters(args).getOrElse("World!")
+
+  println(buildString(hello, world))
 }
